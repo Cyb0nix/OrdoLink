8cef88c36979be4541f861963127527f56958963
Arthur <arrhur.buisson@gmail.com>
2023-06-20 17:52:53 +0200

    header + footer homepage

--- Vue/src/views/HomeView.vue
+++ Vue/src/views/HomeView.vue
@@ -1,9 +1,29 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+ import HeaderHome from '../components/HeaderHome.vue'; 
+ import FooterHome from '../components/FooterHome.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <HeaderHome />
+     
+    <h2>TEST</h2>
+    <FooterHome />
   </main>
 </template>
+
+<style scoped>
+  main {
+    padding: 0 0 40px;
+    background-color: white;
+  }
+
+  h2 {
+    margin: 40px 0 0;
+    font-weight: 400;
+    text-align: center;
+    color: #000;
+  }
+
+
+</style>
