17ab19f1d610351f4c03b6c6d9a6cddc4e30c7f7
Cyb0nix <116296259+Cyb0nix@users.noreply.github.com>
2023-07-05 15:23:08 +0200

    update bdd

--- DB/OrdoLink.sql
+++ DB/OrdoLink.sql
@@ -1,110 +1,126 @@
-CREATE TABLE Patient(
-   id uuid DEFAULT gen_random_uuid(),
-   num_secu INT NOT NULL,
-   lastname VARCHAR(50) NOT NULL,
-   surname VARCHAR(50) NOT NULL,
-   PRIMARY KEY(id),
-   UNIQUE(num_secu)
-);
-
-CREATE TABLE Pharmatien(
-   id uuid DEFAULT gen_random_uuid(),
-   lastname VARCHAR(50) NOT NULL,
-   firstname VARCHAR(50) NOT NULL,
-   rpps INT NOT NULL,
-   PRIMARY KEY(id)
-);
-
-CREATE TABLE MedecinType(
-   id uuid DEFAULT gen_random_uuid(),
-   medecin_type VARCHAR(50) NOT NULL,
-   PRIMARY KEY(id),
-   UNIQUE(medecin_type)
-);
-
-CREATE TABLE Medecin(
-   id uuid DEFAULT gen_random_uuid(),
-   rpps VARCHAR(12) NOT NULL,
-   phone_number VARCHAR(50) NOT NULL,
-   lastname VARCHAR(50) NOT NULL,
-   firstname VARCHAR(50) NOT NULL,
-   adresse VARCHAR(50) NOT NULL,
-   name VARCHAR(50) NOT NULL,
-   rpps_expiration DATE NOT NULL,
-   type_id INT NOT NULL,
-   PRIMARY KEY(id),
-   UNIQUE(rpps),
-   UNIQUE(phone_number),
-   FOREIGN KEY(type_id) REFERENCES MedecinType(id)
-);
-
-CREATE TABLE Ordonnance(
-   id uuid DEFAULT gen_random_uuid(),
-   prescription_date DATE NOT NULL,
-   medecin_id INT NOT NULL,
-   PRIMARY KEY(id),
-   FOREIGN KEY(medecin_id) REFERENCES Medecin(id)
-);
-
-CREATE TABLE Prescription(
-   id uuid DEFAULT gen_random_uuid(),
-   medecine VARCHAR(50) NOT NULL,
-   presciption VARCHAR(50) NOT NULL,
-   renewale INT NOT NULL,
-   given BOOLEAN NOT NULL,
-   ordennance_id INT NOT NULL,
-   PRIMARY KEY(id),
-   FOREIGN KEY(ordennance_id) REFERENCES Ordonnance(id)
-);
-
-CREATE TABLE User_client(
-   id uuid DEFAULT gen_random_uuid(),
-   mail VARCHAR(50) NOT NULL,
-   Password VARCHAR(50) NOT NULL,
-   pwd_is_tmp BOOLEAN NOT NULL,
-   tmp_pwd_creation_date DATE NOT NULL,
-   pharmatien_id INT NOT NULL,
-   patient_id INT NOT NULL,
-   medecin_id INT NOT NULL,
-   PRIMARY KEY(id),
-   UNIQUE(pharmatien_id),
-   UNIQUE(patient_id),
-   UNIQUE(medecin_id),
-   UNIQUE(mail),
-   FOREIGN KEY(pharmatien_id) REFERENCES Pharmatien(id),
-   FOREIGN KEY(patient_id) REFERENCES Patient(id),
-   FOREIGN KEY(medecin_id) REFERENCES Medecin(id)
-);
-
--- edit
-CREATE TABLE Liste_Medecin_Patient(
-   medecin_id INT,
-   patient_id INT,
-   PRIMARY KEY(medecin_id, patient_id),
-   FOREIGN KEY(medecin_id) REFERENCES Medecin(id),
-   FOREIGN KEY(patient_id) REFERENCES Patient(id)
-);
-
-create table account_type
+create table if not exists patient
 (
-    user_id uuid not null
-        constraint account_type_users_id_fk
-            references users,
-    type_id uuid not null
-        constraint type
-            references medecin
-        constraint type2
-            references patient
-        constraint type3
-            references pharmatien,
-    constraint id
-        primary key (user_id, type_id)
+    id        uuid default gen_random_uuid() not null,
+    num_secu  bigint                         not null,
+    lastname  varchar(50)                    not null,
+    firstname varchar(50)                    not null,
+    primary key (id),
+    unique (num_secu)
 );
 
-CREATE TABLE Ordonnance_Consultable(
-   patient_id INT,
-   ordennance_id INT,
-   PRIMARY KEY(patient_id, ordennance_id),
-   FOREIGN KEY(patient_id) REFERENCES Patient(id),
-   FOREIGN KEY(ordennance_id) REFERENCES Ordonnance(id)
+create table if not exists pharmacien
+(
+    id              uuid default gen_random_uuid() not null,
+    lastname        varchar(50)                    not null,
+    firstname       varchar(50)                    not null,
+    rpps            bigint                         not null,
+    rpps_expiration date,
+    constraint pharmatien_pkey
+        primary key (id)
 );
+
+create table if not exists medecintype
+(
+    id           uuid default gen_random_uuid() not null,
+    medecin_type varchar(50)                    not null,
+    primary key (id),
+    unique (medecin_type)
+);
+
+create table if not exists medecin
+(
+    id              uuid default gen_random_uuid() not null,
+    rpps            varchar(12)                    not null,
+    phone_number    varchar(50)                    not null,
+    lastname        varchar(50)                    not null,
+    firstname       varchar(50)                    not null,
+    adresse         varchar(50)                    not null,
+    name            varchar(50)                    not null,
+    rpps_expiration date                           not null,
+    type_id         uuid,
+    primary key (id),
+    unique (rpps),
+    unique (phone_number),
+    constraint type_id
+        foreign key (type_id) references medecintype
+);
+
+create table if not exists ordonnance
+(
+    id                uuid default gen_random_uuid() not null,
+    prescription_date date                           not null,
+    medecin_id        uuid                           not null,
+    patient_id        uuid,
+    primary key (id),
+    foreign key (medecin_id) references medecin
+        on update cascade on delete cascade,
+    constraint ordonnance_patient_id_fk
+        foreign key (patient_id) references patient
+);
+
+create table if not exists prescription
+(
+    id            uuid default gen_random_uuid() not null,
+    medecine      varchar(50)                    not null,
+    posologie     varchar(300)                   not null,
+    renewale      integer                        not null,
+    given         boolean                        not null,
+    ordonnance_id uuid                           not null,
+    quantity      integer,
+    primary key (id),
+    constraint prescription_ordennance_id_fkey
+        foreign key (ordonnance_id) references ordonnance
+            on update cascade on delete cascade
+);
+
+create table if not exists users
+(
+    id                    uuid default gen_random_uuid() not null,
+    email                 varchar(50)                    not null,
+    password              varchar(200)                   not null,
+    pwd_is_tmp            boolean                        not null,
+    tmp_pwd_creation_date timestamp,
+    admin                 boolean,
+    constraint user_client_pkey
+        primary key (id),
+    constraint user_client_mail_key
+        unique (email)
+);
+
+create table if not exists liste_medecin_patient
+(
+    medecin_id uuid not null,
+    patient_id uuid not null,
+    primary key (medecin_id, patient_id),
+    foreign key (medecin_id) references medecin
+        on update cascade on delete cascade,
+    foreign key (patient_id) references patient
+        on update cascade on delete cascade
+);
+
+create table if not exists account_type
+(
+    user_id uuid        not null,
+    type_id uuid        not null,
+    type    varchar(50) not null,
+    constraint id
+        primary key (user_id, type_id),
+    constraint account_type_users_id_fk
+        foreign key (user_id) references users
+            on update cascade on delete cascade
+);
+
+create unique index if not exists account_type_user_id_uindex
+    on account_type (user_id);
+
+create table if not exists tokens
+(
+    id              uuid default gen_random_uuid() not null,
+    user_id         uuid                           not null,
+    token           uuid                           not null,
+    expiration_date timestamp                      not null,
+    constraint tokens_users_id_fk
+        foreign key (user_id) references users
+            on update cascade on delete cascade
+);
+
