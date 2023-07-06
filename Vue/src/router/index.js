ac1464577a7535a5af16a5ee3b91ebf794cd6a46
Arthur <arrhur.buisson@gmail.com>
2023-07-06 09:27:34 +0200

    fix problem with github.pages

--- Vue/src/router/index.js
+++ Vue/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 import { config } from '../../config.js'
 import HomeView from '../views/HomeView.vue'
 import LoginPage from '../views/LoginPage.vue'
@@ -32,71 +32,69 @@ async function routGard(to, from, next) {   //ne marche pas
   }
 }
 
-
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/Login',
-    name: 'LoginPage',
-    component: LoginPage
-  },
-  {
-    path: '/patient',
-    name: 'patient',
-    component: PatientHome,
-    // beforeEnter: routGard
-  },
-  {
-    path: '/medecin/liste-ordonnance/:id',
-    name: 'medecin-liste-ordonnance',
-    component: MedecinListeOrdonnance,
-    // beforeEnter: routGard
-  },
-  {
-    path: '/medecin/liste-patient',
-    name: 'medecin-liste-patient',
-    component: MedecinListePatient,
-    // beforeEnter: routGard
-  },
-  {
-    path: '/medecin/ordonnance/:id',
-    name: 'medecin-ordonnance',
-    component: MedecinCreerOrdonnance,
-    // beforeEnter: routGard
-  },
-  {
-    path: '/admin/creer-compte',
-    name: 'admin-creer-compte',
-    component: AdminCreerCompte,
-    // beforeEnter: routGard
-  },
-  {
-    path: '/ordonnance/:id',
-    name: 'afficher-ordonnance',
-    component: AfficherOrdonnance,
-    // beforeEnter: routGard
-  },
-  {
-    path: '/pharmacien',
-    name: 'pharmacien',
-    component: Pharmacien,
-    // beforeEnter: routGard
-  },
-  {
-    path: '/admin',
-    name: 'admin-liste-user',
-    component: AdminListUser,
-    // beforeEnter: routsGard
-  },
-]
-
 const router = createRouter({
-  history: createWebHashHistory(process.env.NODE_ENV === 'production' ? config.base_url : '/'),
-  routes
+  history: createWebHistory('/OrdoLink/'),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/Login',
+      name: 'LoginPage',
+      component: LoginPage
+    },
+    {
+      path: '/patient',
+      name: 'patient',
+      component: PatientHome,
+      // beforeEnter: routGard
+    },
+    {
+      path: '/medecin/liste-ordonnance/:id',
+      name: 'medecin-liste-ordonnance',
+      component: MedecinListeOrdonnance,
+      // beforeEnter: routGard
+    },
+    {
+      path: '/medecin/liste-patient',
+      name: 'medecin-liste-patient',
+      component: MedecinListePatient,
+      // beforeEnter: routGard
+    },
+    {
+      path: '/medecin/ordonnance/:id',
+      name: 'medecin-ordonnance',
+      component: MedecinCreerOrdonnance,
+      // beforeEnter: routGard
+    },
+    {
+      path: '/admin/creer-compte',
+      name: 'admin-creer-compte',
+      component: AdminCreerCompte,
+      // beforeEnter: routGard
+    },
+    {
+      path: '/ordonnance/:id',
+      name: 'afficher-ordonnance',
+      component: AfficherOrdonnance,
+      // beforeEnter: routGard
+    },
+    {
+      path: '/pharmacien',
+      name: 'pharmacien',
+      component: Pharmacien,
+      // beforeEnter: routGard
+    },
+    {
+      path: '/admin',
+      name: 'admin-liste-user',
+      component: AdminListUser,
+      // beforeEnter: routsGard
+    },
+  ]
 })
+
 
 export default router
